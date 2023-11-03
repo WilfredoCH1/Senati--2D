28ebfb67bebb1dcbd86a890cf36c3214d0e82ee4
JASMANI-17 <jasmanillerenal16@gmail.com>
2023-11-02 19:11:15 -0500

    --
    --

---
+++ Assets/CaontrolDeCamara.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Source
+{
+    public class CaontrolDeCamara : MonoBehaviour
+    {
+        public Transform personaje; // El transform del personaje 2D que seguirá la cámara
+        public float suavizado = 5f; // Controla la suavidad del seguimiento de la cámara
+
+        private Vector3 offset; // La distancia inicial entre la cámara y el personaje
+
+        void Start()
+        {
+            offset = transform.position - personaje.position;
+        }
+
+        void FixedUpdate()
+        {
+            Vector3 targetPosition = personaje.position + offset;
+            transform.position = Vector3.Lerp(transform.position, targetPosition, suavizado * Time.deltaTime);
+        }
+    }
+
+}
+
